6553b9905f2e2e434b7649865a7b7759240ffaf4
Philip Chia <pchia.e@gmail.com>
2019-03-30 09:10:29 -0700

    New File

---
+++ gardenspider_sandbox.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+#
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Camera image classification demo code.
+
+Runs continuous image classification on camera frames and prints detected object
+classes.
+
+Example:
+image_classification_camera.py --num_frames 10
+"""
+import argparse
+import contextlib
+import servo
+import time
+from aiy.vision.inference import CameraInference
+from aiy.vision.models import object_detection
+from picamera import PiCamera
+from aiy.vision.annotator import Annotator
+
+def classes_info(classes):
+    return ', '.join('%s (%.2f)' % pair for pair in classes)
+
+@contextlib.contextmanager
+def CameraPreview(camera, enabled):
+    if enabled:
+        camera.start_preview()
+    try:
+        yield
+    finally:
+        if enabled:
+            camera.stop_preview()
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser('Image classification camera inference example.')
+    parser.add_argument('--num_frames', '-n', type=int, default=None,
+        help='Sets the number of frames to run for, otherwise runs forever.')
+    parser.add_argument('--num_objects', '-c', type=int, default=3,
+        help='Sets the number of object interences to print.')
+    parser.add_argument('--nopreview', dest='preview', action='store_false', default=True,
+        help='Enable camera preview')
+    args = parser.parse_args()
+
+    with PiCamera(sensor_mode=4, framerate=30, resolution=(1640, 1232)) as camera, \
+         CameraPreview(camera, enabled=args.preview), \
+         CameraInference(object_detection.model()) as inference:
+        annotator = Annotator(camera, dimensions=(320, 240))
+        scale_x = 320 / 1640
+        scale_y = 240 / 1232    
+        
+        def transform(bounding_box):
+            x, y, width, height = bounding_box
+            return (scale_x * x, scale_y * y, scale_x * (x + width),
+                    scale_y * (y + height))        
+        
+        
+        
+        for result in inference.run(args.num_frames):
+            objs = object_detection.get_objects(result, threshold=0.3, offset=(0, 0))
+#            objs = object_detection.get_classes(result)
+            print (objs)
+#
+            annotator.clear()
+            for obj in objs:
+                print (obj.bounding_box[0])
+                x0, y0, width, height = obj.bounding_box
+                x = float((x0 + width/2) - 1640/2) #x range is -820 to 820
+                y = float(1232/2 - (y0 + height/2)) #y range is -616 to 616
+                print (obj.kind)
+                print (obj.score)
+                if obj.kind == 1 and obj.score > 0.5:
+                    servo.triwalk(x/41) #820/20
+#                    servo.rotate(x/18.222) #820/45
+                    print('xval: %f', x/41)
+                     
+#                if obj.kind == 1 and obj.score > 0.5 and -100 <= x <= 100:
+                    
+#                annotator.bounding_box(transform(obj.bounding_box), fill=0)
+#                
+#            annotator.update()
+#            if objs:
+#                obj = objs[0]
+#                x, y, width, height = obj.bounding_box
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#                print (obj.bounding_box)
+#                camera.annotate_text = '%s' % objs[0]
+#                print (camera.annotate_text)
+#                if camera.annotate_text == 'mountain bike/all-terrain bike/off-roader':
+#                    print ("bike!")
+
+#servo.zero(0)
+#time.sleep(1)
+#servo.standup()   
+#for x in range(4):
+#    servo.triwalk(15)
+#for x in range(4):
+#    servo.rotate('r')    
+#for x in range(4):
+#    servo.rotate('l')   
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ servo_sandbox.py
@@ -0,0 +1,496 @@
+import time
+from adafruit_servokit import ServoKit
+
+
+kit = [ServoKit(address=0x40, channels=16), ServoKit(address=0x41, channels=16)]
+
+
+for c in range(16):
+    kit[0].servo[c].actuation_range = 270
+    kit[0].servo[c].set_pulse_width_range(500, 2500)
+    kit[1].servo[c].actuation_range = 270
+    kit[1].servo[c].set_pulse_width_range(500, 2500)
+    
+speed = 0.25
+
+RB = []
+RB.extend((kit[0].servo[0], kit[0].servo[1], kit[0].servo[2]))
+RM = []
+RM.extend((kit[0].servo[3], kit[0].servo[4], kit[0].servo[5]))
+RF = []
+RF.extend((kit[0].servo[6], kit[0].servo[7], kit[0].servo[8]))
+
+LF = []
+LF.extend((kit[1].servo[0], kit[1].servo[1], kit[1].servo[2]))
+LM = []
+LM.extend((kit[1].servo[3], kit[1].servo[4], kit[1].servo[5]))
+LB = []
+LB.extend((kit[1].servo[6], kit[1].servo[7], kit[1].servo[8]))
+
+joint0 = [LB[0],LM[0],LF[0],RB[0],RM[0],RF[0]]
+joint1 = [LB[1],LM[1],LF[1],RB[1],RM[1],RF[1]]
+joint2 = [LB[2],LM[2],LF[2],RB[2],RM[2],RF[2]]
+
+
+
+offset = -135
+# Setting limits on movement ranges
+
+
+max_angle_joint0 = 30
+min_angle_joint0 = -30
+max_angle_joint1 = 100
+min_angle_joint1 = -100
+max_angle_joint2 = 120
+min_angle_joint2 = -120
+
+
+
+#def initalize():
+#    c = 0
+#    for c in range(16):
+#        if c%3 == 0:
+#            kit[0].servo[c].angle = 135
+#            kit[1].servo[c].angle = 135
+#        else:
+#            kit[0].servo[c].angle = 180
+#            kit[1].servo[c].angle = 90	 
+#    time.sleep(1)
+    
+
+def seg(joint,deg):
+# Enforcing the limits on movement ranges
+    if joint in joint0 and deg > max_angle_joint0:
+        deg = max_angle_joint0    
+    if joint in joint0  and deg < min_angle_joint0:
+        deg = min_angle_joint0
+    if joint in joint1 and deg > max_angle_joint1:
+        deg = max_angle_joint1    
+    if joint in joint1  and deg < min_angle_joint1:
+        deg = min_angle_joint1
+    if joint in joint2 and deg > max_angle_joint2:
+        deg = max_angle_joint2    
+    if joint in joint2  and deg < min_angle_joint2:
+        deg = min_angle_joint2    
+       
+# Moving joints with tuning      
+    if joint == LB[0]:
+        tuning = 0
+        joint.angle = abs(offset - deg - tuning)        
+    if joint == LB[1]:
+        tuning = 6
+        joint.angle = abs(offset - deg - tuning)
+    if joint == LB[2]:
+        tuning = 8
+        joint.angle = abs(offset - deg - tuning)        
+        
+    if joint == LM[0]:
+        tuning = 0
+        joint.angle = abs(offset - deg - tuning)        
+    if joint == LM[1]:
+        tuning = 8
+        joint.angle = abs(offset - deg - tuning)
+    if joint == LM[2]:
+        tuning = 3
+        joint.angle = abs(offset - deg - tuning) 
+
+    if joint == LF[0]:
+        tuning = 0
+        joint.angle = abs(offset - deg - tuning)        
+    if joint == LF[1]:
+        tuning = 2
+        joint.angle = abs(offset - deg - tuning)
+    if joint == LF[2]:
+        tuning = 0
+        joint.angle = abs(offset - deg - tuning) 
+
+    if joint == RB[0]:
+        tuning = 0 
+        joint.angle = abs(offset + deg + tuning)        
+    if joint == RB[1]:
+        tuning = 0 
+        joint.angle = abs(offset + deg + tuning)   
+    if joint == RB[2]:
+        tuning = 7 
+        joint.angle = abs(offset + deg + tuning)    
+        
+    if joint == RM[0]:
+        tuning = 0 
+        joint.angle = abs(offset + deg + tuning)        
+    if joint == RM[1]:
+        tuning = 5 
+        joint.angle = abs(offset + deg + tuning)   
+    if joint == RM[2]:
+        tuning = 3 
+        joint.angle = abs(offset + deg + tuning)            
+        
+    if joint == RF[0]:
+        tuning = 0 
+        joint.angle = abs(offset + deg + tuning)        
+    if joint == RF[1]:
+        tuning = 6
+        joint.angle = abs(offset + deg + tuning)   
+    if joint == RF[2]:
+        tuning = 2 
+        joint.angle = abs(offset + deg + tuning)                  
+
+
+
+def triwalk(drift):
+    #if positive, walk straight with slight right drift and vice versa
+    if drift > 0: #moving right
+        R_drift = -drift
+        L_drift = 0
+    if drift < 0: #moving left
+        R_drift = 0
+        L_drift = drift
+    elif drift == 0:
+        R_drift = 0
+        L_drift = 0
+        
+#Raise and Pivot legs LF LB  RM  
+    seg(LF[0],20 + L_drift)
+    seg(LF[1],10)
+    seg(LF[2],-90)    
+    
+    seg(LB[0],20 + L_drift)
+    seg(LB[1],10)
+    seg(LB[2],-90)
+    
+    seg(RM[0],20 + R_drift)
+    seg(RM[1],10)
+    seg(RM[2],-90)
+    
+# Pivot legs LM RB RF      
+    seg(LM[0],-20 - L_drift)
+    seg(LM[1],-30)
+    seg(LM[2],-60)
+        
+    seg(RB[0],-20 - R_drift)
+    seg(RB[1],-30)
+    seg(RB[2],-60)
+     
+    seg(RF[0],-20 - R_drift)
+    seg(RF[1],-30)
+    seg(RF[2],-60)
+        
+    time.sleep(speed)
+                
+#Lower legs LF LB RM  
+    seg(LF[0],20 + L_drift)
+    seg(LF[1],-30)
+    seg(LF[2],-60)
+       
+    seg(LB[0],20 + L_drift)
+    seg(LB[1],-30)
+    seg(LB[2],-60)
+     
+    seg(RM[0],20 + R_drift)
+    seg(RM[1],-30)
+    seg(RM[2],-60)
+        
+    time.sleep(speed)
+        
+#Raise and Pivot legs LM RB RF
+    seg(LM[0],20 + L_drift)
+    seg(LM[1],10)
+    seg(LM[2],-90)
+        
+    seg(RB[0],20 + R_drift)
+    seg(RB[1],10)
+    seg(RB[2],-90)
+     
+    seg(RF[0],20 + R_drift)
+    seg(RF[1],10)
+    seg(RF[2],-90)
+        
+# Pivot legs LF LB RM      
+    seg(LF[0],-20 - L_drift)
+    seg(LF[1],-30)
+    seg(LF[2],-60)
+       
+    seg(LB[0],-20 - L_drift)
+    seg(LB[1],-30)
+    seg(LB[2],-60)
+     
+    seg(RM[0],-20 - R_drift)
+    seg(RM[1],-30)
+    seg(RM[2],-60)
+    
+    time.sleep(speed)      
+    
+#Lower legs LM RB RF   
+    seg(LM[0],20 + L_drift)
+    seg(LM[1],-30)
+    seg(LM[2],-60)
+        
+    seg(RB[0],20 + R_drift)
+    seg(RB[1],-30)
+    seg(RB[2],-60)
+     
+    seg(RF[0],20 + R_drift)
+    seg(RF[1],-30)
+    seg(RF[2],-60)
+    
+    time.sleep(speed)   
+            
+def rotate(direction):
+    if direction > 45: #limit max
+        direction = 45
+    if direction < -45: #limit min
+        direction = -45
+
+
+    r = -float(direction)/45
+    l = float(direction)/45
+
+     
+    
+    
+#    if direction == 'R' or direction == 'r' or direction == 'right' or direction == 'Right':
+#        r = -1
+#        l = 1
+#    if direction == 'L' or direction == 'l' or direction == 'left' or direction == 'Left':
+#        r = 1
+#        l = -1
+
+            
+#Raise and Pivot legs LF LB  RM  
+    seg(LF[0],20 * l)
+    seg(LF[1],10)
+    seg(LF[2],-90)    
+    
+    seg(LB[0],20 * l)
+    seg(LB[1],10)
+    seg(LB[2],-90)
+    
+    seg(RM[0],20 * r)
+    seg(RM[1],10)
+    seg(RM[2],-90)
+    
+# Pivot legs LM RB RF      
+    seg(LM[0],-20 * l)
+    seg(LM[1],-30)
+    seg(LM[2],-60)
+        
+    seg(RB[0],-20 * r)
+    seg(RB[1],-30)
+    seg(RB[2],-60)
+     
+    seg(RF[0],-20 * r)
+    seg(RF[1],-30)
+    seg(RF[2],-60)
+        
+    time.sleep(speed)
+                
+#Lower legs LF LB RM  
+    seg(LF[0],20 * l)
+    seg(LF[1],-30)
+    seg(LF[2],-60)
+       
+    seg(LB[0],20 * l)
+    seg(LB[1],-30)
+    seg(LB[2],-60)
+     
+    seg(RM[0],20 * r)
+    seg(RM[1],-30)
+    seg(RM[2],-60)
+        
+    time.sleep(speed)
+        
+#Raise and Pivot legs LM RB RF
+    seg(LM[0],20 * l)
+    seg(LM[1],10)
+    seg(LM[2],-90)
+        
+    seg(RB[0],20 * r)
+    seg(RB[1],10)
+    seg(RB[2],-90)
+     
+    seg(RF[0],20 * r)
+    seg(RF[1],10)
+    seg(RF[2],-90)
+        
+# Pivot legs LF LB RM      
+    seg(LF[0],-20 * l)
+    seg(LF[1],-30)
+    seg(LF[2],-60)
+       
+    seg(LB[0],-20 * l)
+    seg(LB[1],-30)
+    seg(LB[2],-60)
+     
+    seg(RM[0],-20 * r)
+    seg(RM[1],-30)
+    seg(RM[2],-60)
+    
+    time.sleep(speed)      
+    
+#Lower legs LM RB RF   
+    seg(LM[0],20 * l)
+    seg(LM[1],-30)
+    seg(LM[2],-60)
+        
+    seg(RB[0],20 * r)
+    seg(RB[1],-30)
+    seg(RB[2],-60)
+     
+    seg(RF[0],20 * r)
+    seg(RF[1],-30)
+    seg(RF[2],-60)
+    
+    time.sleep(speed)    
+    
+    
+##Raise and Pivot legs A C & E
+#    seg(LF[0],25)
+#    seg(LF[1],-20)
+#    seg(LF[2],-45)
+#       
+#    seg(LB[0],25)
+#    seg(LB[1],-20)
+#    seg(LB[2],-45)
+#     
+#    seg(RM[0],25)
+#    seg(RM[1],-20)
+#    seg(RM[2],45)
+#        
+##Pivot legs B D & F
+#    seg(LM[0],0)
+#    seg(LM[1],45)
+#    seg(LM[2],-45)
+#        
+#    seg(RB[0],0)
+#    seg(RB[1],45)
+#    seg(RB[2],45)
+#     
+#    seg(RF[0],0)
+#    seg(RF[1],45)
+#    seg(RF[2],45)
+#
+#    time.sleep(speed)     
+#        
+##Raise and Pivot legs B D & F
+#    seg(LM[0],25)
+#    seg(LM[1],0)
+#    seg(LM[2],-45)
+#        
+#    seg(RB[0],25)
+#    seg(RB[1],0)
+#    seg(RB[2],45)
+#     
+#    seg(RF[0],25)
+#    seg(RF[1],0)
+#    seg(RF[2],45)
+#        
+##Lower legs A C & E    
+#    seg(LF[0],25)
+#    seg(LF[1],45)
+#    seg(LF[2],-45)
+#       
+#    seg(LB[0],25)
+#    seg(LB[1],45)
+#    seg(LB[2],-45)
+#     
+#    seg(RM[0],25)
+#    seg(RM[1],45)
+#    seg(RM[2],45)
+#        
+#    time.sleep(speed)     
+#    
+##Lower legs B D & F
+#    seg(LM[0],25)
+#    seg(LM[1],45)
+#    seg(LM[2],-45)
+#        
+#    seg(RB[0],25)
+#    seg(RB[1],45)
+#    seg(RB[2],45)
+#     
+#    seg(RF[0],25)
+#    seg(RF[1],45)
+#    seg(RF[2],45)
+#        
+##Pivot legs A C & E    
+#    seg(LF[0],0)
+#    seg(LF[1],45)
+#    seg(LF[2],-45)
+#       
+#    seg(LB[0],0)
+#    seg(LB[1],45)
+#    seg(LB[2],-45)
+#     
+#    seg(RM[0],0)
+#    seg(RM[1],45)
+#    seg(RM[2],45)
+#        
+#    time.sleep(speed)  
+
+    
+    
+def test(leg):
+    seg(leg,0)
+    time.sleep(0.25)
+    print("segment is moving!")
+    seg(leg,10)
+    time.sleep(0.25)
+    seg(leg,-10)
+    time.sleep(0.25)    
+    seg(leg,0)
+    time.sleep(0.25)   
+    
+def zero(x):  
+    c = 0
+    for c in range(3):
+        seg(RB[c],x)
+        seg(RM[c],x)
+        seg(RF[c],x)
+        
+        seg(LB[c],x)
+        seg(LM[c],x)
+        seg(LF[c],x)
+#        kit[0].servo[c].angle = 135
+#        kit[1].servo[c].angle = 135
+
+def standup():
+    for j1 in joint1:
+        seg(j1,90)
+    for j2 in joint2:
+        seg(j2,-110)
+    time.sleep(1)
+    
+    for j1 in joint1:
+        seg(j1,-30)
+    time.sleep(0.25)
+    for j2 in joint2:
+        seg(j2,-60)
+    time.sleep(1)
+    
+
+#zero(0)
+#time.sleep(1)
+#standup()   
+#for x in range(4):
+#    triwalk(-20)
+#for x in range(4):
+#    triwalk(20)    
+#for x in range(2):
+#    rotate(45)    
+#for x in range(2):
+#    rotate(-45)    
+#for x in range(2):
+#    rotate(22)    
+#for x in range(2):
+#    rotate(-22)    
+
+#    
+
+#while True:  
+#    segment = 0
+#    x ='segment'    
+#    exec("%s = %s" % (x,input("enter segment ")))
+#    test(segment)
+#zero()
+
+
+#initalize()
