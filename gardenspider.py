56d1d79c835b5a0e712767fc8a2ec96e4b18341c
Philip Chia <pchia.e@gmail.com>
2019-04-01 21:50:27 -0700

    Update gardenspider.py
    Threading fixed

--- gardenspider.py
+++ gardenspider.py
@@ -12,14 +12,13 @@ import contextlib
 import servo
 import LED
 import time
-from multiprocessing import Process
-#from concurrent.futures import ThreadPoolExecutor
-#import _thread
-#from threading import Thread
+from threading import Thread, Semaphore
 from aiy.vision.inference import CameraInference
 from aiy.vision.models import object_detection
 from picamera import PiCamera
 from aiy.vision.annotator import Annotator
+
+sem = Semaphore()
 
 def classes_info(classes):
     return ', '.join('%s (%.2f)' % pair for pair in classes)
@@ -49,15 +48,14 @@ def main():
     with PiCamera(sensor_mode=4, framerate=30, resolution=(1640, 1232)) as camera, \
          CameraPreview(camera, enabled=args.preview), \
          CameraInference(object_detection.model()) as inference:
-#        annotator = Annotator(camera, dimensions=(320, 240))
-#        scale_x = 320 / 1640
-#        scale_y = 240 / 1232    
+        annotator = Annotator(camera, dimensions=(320, 240))
+        scale_x = 320 / 1640
+        scale_y = 240 / 1232    
         
-#        def transform(bounding_box):
-#            x, y, width, height = bounding_box
-#            return (scale_x * x, scale_y * y, scale_x * (x + width),
-#                    scale_y * (y + height))        
-
+        def transform(bounding_box):
+            x, y, width, height = bounding_box
+            return (scale_x * x, scale_y * y, scale_x * (x + width),
+                    scale_y * (y + height))        
         for result in inference.run(args.num_frames):
             objs = object_detection.get_objects(result, threshold=0.3, offset=(0, 0))
             print (objs)
@@ -68,23 +66,26 @@ def main():
             if objs:
                 obj = objs[0]
                 x, y, width, height = obj.bounding_box
-                                
+                
+                
                 print (obj.bounding_box[0])
                 x0, y0, width, height = obj.bounding_box
                 x = float((x0 + width/2) - 1640/2) #x range is -820 to 820
                 y = float(1232/2 - (y0 + height/2)) #y range is -616 to 616
                 print (obj.kind)
                 print (obj.score)
-
-#                pool = ThreadPoolExecutor(5)               
+                
                 if obj.kind == 1 and obj.score > 0.5: 
                     LED.color(0,0,255)
-                    Process(target=servo.triwalk, args=(x/41,))
-#                   future = pool.submit(servo.triwalk, (x/41))
+                    if -400 < x < 400:
+                        t = Thread(target=servo.triwalk, args = (x/41,))
+                    else:
+                        t = Thread(target=servo.rotate, args = (x/18.222,))
+                        print('rotate!')
+                    t.start()
+                    time.sleep(.3)
+                        
                     
-#                    _thread.start_new_thread(servo.triwalk,(x/41,)) #820/20
-#                    _thread.start_new_thread(LED.color,(0,0,255,))
-#                    _thread.start_new_thread(servo.rotate,(x/18.222,)) #820/45
 
 #                    servo.triwalk(x/41) #820/20
 #                    print('xval: %f', x/41)
@@ -94,7 +95,8 @@ def main():
                         LED.color(0,255,0) #Green = I'm aiming at you
                 else:
                     LED.color(255,0,0) #Red = where are you?
-
+                
+#                t.join()
 
 
 
